d72dfcd96dcce69d2fa890e0c76b9fa1fa60d7d0
BogdanStronchyk <yorkshir2000@gmail.com>
2022-10-16 20:00:50 +0300

    Raycast shooting
    -bullets are no more used as game objects. Raycasting was implemented

--- Assets/Scrypts/BulletFire.cs
+++ Assets/Scrypts/BulletFire.cs
@@ -8,30 +8,21 @@ public class BulletFire : MonoBehaviour
     public float fireRate;
     private bool isShooting;
 
-
-    private void ActivateBullet()
-    {
-        GameObject obj = ObjectPoolerScript.current.GetPooledObject();
-        if (obj == null) return;
-
-        obj.transform.position = GameObject.Find("BuletSpawner").transform.position;
-        obj.transform.rotation = GameObject.Find("BuletSpawner").transform.rotation;
-        obj.GetComponent<BulletBehavior>().flightDirection = GetFlightDirection();
-        obj.SetActive(true);
-    }
-
-    private Vector3 GetFlightDirection()
+    private void Shoot()
     {
         Ray ray = FindObjectOfType<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-        Vector3 flightDirection = ray.direction.normalized;
-        return flightDirection;
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log($"You hit {hit.rigidbody}");
+        }
     }
 
     public void Fire()
     {
         if (isShooting)
         {
-            InvokeRepeating("ActivateBullet", 0f, fireRate);
+            InvokeRepeating("Shoot", 0f, fireRate);
             isShooting = false;
         }
     }
